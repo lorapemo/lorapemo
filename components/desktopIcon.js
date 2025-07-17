abb6ee14dc4d305c4420d9113ef750d3660793b9
Lorenzo Pérez <lorapemo@gmail.com>
2025-07-16 19:30:34 -0600

    fix: now the handleWindow function gets activated only for the icon/element that was clicked
    Before: any time an element with a particular class got clicked->it triggered to open all the contents for the elements

--- components/desktopIcon.js
+++ components/desktopIcon.js
@@ -1,11 +1,11 @@
 import { handleWindow } from "../utilities/handleWindow.js"
 
 export function desktopIcon(img, title, dataWindow, content = "Hello World") {
-    $(document).on('click', '.desktop-icon', function () {
+    $(document).on('click', `#${dataWindow}`, function () {
         handleWindow(title, content)
     });
     return (`
-        <div class="desktop-icon" data-window="${dataWindow}" >
+        <div class="desktop-icon" data-window="${dataWindow}" id="${dataWindow}">
             <img src="${img}" alt="Document">
             <span>${title}</span>
         </div>
