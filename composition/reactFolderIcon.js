89a2455f6b11d7845574916ec28c4d1a3aada57a
Lorenzo Pérez <lorapemo@gmail.com>
2025-07-16 19:32:19 -0600

    feat: added folder for react projects

---
+++ composition/reactFolderIcon.js
@@ -0,0 +1,11 @@
+import { desktopIcon } from '../components/desktopIcon.js'
+
+export function reactFolderIcon(){
+    const content = ``
+
+    return(
+        `
+        ${desktopIcon("https://raw.githubusercontent.com/lorapemo/lorapemo/refs/heads/dev/public/reactFolder.png", "React", "react_folder_icon", content)}
+        `
+    )
+}
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-import { readmeIcon } from './composition/readmeIcon.js'
-
+import { readmeIcon } from './composition/readmeIcon.js';
+import { reactFolderIcon } from './composition/reactFolderIcon.js';
 document.getElementById("desktop-icons").innerHTML = `
     ${readmeIcon()}
+    ${reactFolderIcon()}
     `
